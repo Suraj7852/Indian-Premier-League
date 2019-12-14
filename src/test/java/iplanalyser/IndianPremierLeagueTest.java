79cb2a333624dbbbdce9c0fa645f7e9926ce218f
Suraj7852 <surajpj7852@gmail.com>
2019-12-14 11:49:17 +0530

    ADDED: test if wrong file path sent throws exception

--- src/test/java/iplanalyser/IndianPremierLeagueTest.java
+++ src/test/java/iplanalyser/IndianPremierLeagueTest.java
@@ -7,15 +7,24 @@ import java.util.Map;
 
 public class IndianPremierLeagueTest {
     private static final String IPL_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
-    
+    private static final String IPL_CSV_WRONG_FILE_PATH = "./src/main/resources/IPL2019FactsheetMostRuns.csv";
+    IndianPremierLeague premierLeague = new IndianPremierLeague();
     @Test
     public void givenIplDataFile_ReturnsPlayerWith_BestBattingAvg() {
-        IndianPremierLeague premierLeague = new IndianPremierLeague();
         try {
             Map<String, IplDTO> stringIplDTOMap = premierLeague.loadIplData(IPL_CSV_FILE_PATH);
             Assert.assertEquals(100,stringIplDTOMap.size());
         } catch (IplAnalyserException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenIplDataFile_ifWrongFilePathSent_ThrowsException() {
+        try {
+            premierLeague.loadIplData(IPL_CSV_WRONG_FILE_PATH);
+        } catch (IplAnalyserException e) {
+            Assert.assertEquals(IplAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
         }
     }
 }
